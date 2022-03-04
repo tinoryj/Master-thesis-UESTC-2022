655d68bdfe4a0f3ecfea5827bd1fe7f838127da4
tinoryj <tinoryj@gmail.com>
2022-03-04 16:22:10 +0800

    fixed publication and figures font

---
+++ pic/crop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function getdir(){
+    echo $1
+    for file in $1/*
+    do
+    if test -f $file
+    then
+        echo $file
+        arr=(${arr[*]} $file)
+    else
+        getdir $file
+    fi
+    done
+}
+getdir /wls
+echo ${arr[@]}
